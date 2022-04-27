1132db6ca2dfed6db3d088041cbce14368486326
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-04-27 20:01:20 +0900

    implement logout

--- node-admin/src/controller/auth.controller.ts
+++ node-admin/src/controller/auth.controller.ts
@@ -78,17 +78,30 @@ export const Login = async (req: Request, res: Response) => {
 };
 
 export const AuthenticatedUser = async (req: Request, res: Response) => {
-  const jwt = req.cookies['jwt'];
+  try {
+    const jwt = req.cookies['jwt'];
 
-  const payload: any = verify(jwt, 'secret');
+    const payload: any = verify(jwt, 'secret');
 
-  if (!payload) {
+    if (!payload) {
+      return res.status(401).send({ message: 'unauthenticated' });
+    }
+
+    const repository = getManager().getRepository(User);
+
+    const { password, ...user } = await repository.findOne(payload.id);
+
+    res.send(user);
+  } catch (e) {
     return res.status(401).send({ message: 'unauthenticated' });
   }
+};
 
-  const repository = getManager().getRepository(User);
-
-  const { password, ...user } = await repository.findOne(payload.id);
-
-  res.send(user);
+export const Logout = async (req: Request, res: Response) => {
+  res.cookie('jwt', '', {
+    maxAge: 0,
+  });
+  res.send({
+    message: 'success',
+  });
 };

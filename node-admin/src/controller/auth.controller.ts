1d5a9ba65e02241d8bcaa4cedc86237625aa49b5
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-04-27 14:39:36 +0900

    create validation type

--- node-admin/src/controller/auth.controller.ts
+++ node-admin/src/controller/auth.controller.ts
@@ -1,5 +1,27 @@
 import { Request, Response } from 'express';
+import { RegisterValidation } from '../validation/register.validation';
+
+interface body {
+  first_name: string;
+  last_name: string;
+  email: string;
+  password: string;
+  password_confirm: string;
+}
 
 export const Register = (req: Request, res: Response) => {
+  const body: body = req.body;
+
+  const { error } = RegisterValidation.validate(body);
+
+  if (error) {
+    return res.status(400).send(error.details);
+  }
+
+  if (body.password !== body.password_confirm) {
+    return res.status(400).send({
+      message: "Password's do not match",
+    });
+  }
   res.send(req.body);
 };

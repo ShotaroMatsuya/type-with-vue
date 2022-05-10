ccb0a80ce5320036f4e9b4fa1b854749844b33ed
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-05-11 01:22:03 +0900

    create Users CRUD

--- node-admin/src/routes.ts
+++ node-admin/src/routes.ts
@@ -10,6 +10,14 @@ import {
 
 import { AuthMiddleware } from './middleware/auth.middleware';
 
+import {
+  Users,
+  CreateUser,
+  GetUser,
+  UpdateUser,
+  DeleteUser,
+} from './controller/user.controller';
+
 export const routes = (router: Router) => {
   router.post('/api/register', Register);
   router.post('/api/login', Login);
@@ -17,4 +25,10 @@ export const routes = (router: Router) => {
   router.post('/api/logout', AuthMiddleware, Logout);
   router.put('/api/users/info', AuthMiddleware, UpdateInfo);
   router.put('/api/users/password', AuthMiddleware, updatePassword);
+
+  router.get('/api/users', AuthMiddleware, Users);
+  router.post('/api/users', AuthMiddleware, CreateUser);
+  router.get('/api/users/:id', AuthMiddleware, GetUser);
+  router.put('/api/users/:id', AuthMiddleware, UpdateUser);
+  router.delete('/api/users/:id', AuthMiddleware, DeleteUser);
 };

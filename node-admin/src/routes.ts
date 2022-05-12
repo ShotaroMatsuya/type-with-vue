b5b95435427216c6c9f24b6a96544ebfa62e3cbf
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-05-13 05:58:00 +0900

    learn how to run raw queries in controller

--- node-admin/src/routes.ts
+++ node-admin/src/routes.ts
@@ -33,7 +33,7 @@ import {
   UpdateProduct,
 } from './controller/product.controller';
 import { Upload } from './controller/image.controller';
-import { Export, Orders } from './controller/order.cotroller';
+import { Chart, Export, Orders } from './controller/order.cotroller';
 
 export const routes = (router: Router) => {
   router.post('/api/register', Register);
@@ -69,4 +69,5 @@ export const routes = (router: Router) => {
   router.get('/api/orders', AuthMiddleware, Orders);
 
   router.post('/api/export', AuthMiddleware, Export);
+  router.get('/api/chart', AuthMiddleware, Chart);
 };

4336af6ff1bc8894477ca23a9a2b57003d7e1bcc
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-06-08 00:01:36 +0900

    implement router guards

--- vue-admin/src/components/HeaderNav.vue
+++ vue-admin/src/components/HeaderNav.vue
@@ -5,33 +5,41 @@
     <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#"
       >Company name</a
     >
-    <button
-      class="navbar-toggler position-absolute d-md-none collapsed"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#sidebarMenu"
-      aria-controls="sidebarMenu"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <input
-      class="form-control form-control-dark w-100"
-      type="text"
-      placeholder="Search"
-      aria-label="Search"
-    />
-    <div class="navbar-nav">
-      <div class="nav-item text-nowrap">
-        <a class="nav-link px-3" href="#">Sign out</a>
-      </div>
-    </div>
+
+    <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+      <router-link
+        to="/profile"
+        class="nav-link px-3 text-white text-decoration-none"
+        >{{ name }}</router-link
+      >
+      <router-link
+        to="/login"
+        class="nav-link px-3 text-white text-decoration-none"
+        @click="logout"
+        >Sign out</router-link
+      >
+    </nav>
   </header>
 </template>
 
-<script>
+<script lang="ts">
+import axios from 'axios';
+
 export default {
   name: 'HeaderNav',
+  data() {
+    return {
+      name: '',
+    };
+  },
+  async mounted() {
+    const { data } = await axios.get('user');
+    this.name = data.first_name + ' ' + data.last_name;
+  },
+  methods: {
+    async logout() {
+      await axios.post('logout');
+    },
+  },
 };
 </script>
--- vue-admin/src/pages/BaseWrapper.vue
+++ vue-admin/src/pages/BaseWrapper.vue
@@ -14,12 +14,24 @@
 <script>
 import HeaderNav from '@/components/HeaderNav.vue';
 import HeaderMenu from '@/components/HeaderMenu.vue';
+import axios from 'axios';
 
 export default {
   name: 'BaseWrapper',
   components: {
     HeaderNav,
     HeaderMenu,
+  },
+  data() {
+    return {};
+  },
+  async mounted() {
+    try {
+      const { data } = await axios.get('user');
+      console.log(data);
+    } catch (e) {
+      this.$router.push('/login');
+    }
   },
 };
 </script>

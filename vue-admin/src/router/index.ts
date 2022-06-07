8fc364e5a0cc36206d0c33d44326f950bcb17740
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-06-07 18:53:38 +0900

    add root routes and users routes

--- vue-admin/src/router/index.ts
+++ vue-admin/src/router/index.ts
@@ -1,6 +1,11 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import DashboardPage from '@/pages/DashboardPage.vue';
+import UsersPage from '@/pages/UsersPage.vue';
 
-const routes: Array<RouteRecordRaw> = [];
+const routes: Array<RouteRecordRaw> = [
+  { path: '', component: DashboardPage },
+  { path: '/users', component: UsersPage },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),

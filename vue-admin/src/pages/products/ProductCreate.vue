2ebd9eaa76a7280694f5ce48e5ccb89d07ada8d1
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-05-18 00:54:37 +0900

    feat: implement C & U for product

---
+++ vue-admin/src/pages/products/ProductCreate.vue
@@ -0,0 +1,50 @@
+<template> 
+    <form @submit.prevent="submit">
+        <div class="mb-3">
+            <label>Title</label>
+            <input v-model="form.title" class="form-control" name="first_name">
+        </div>
+        <div class="mb-3">
+            <label>Description</label>
+            <textarea v-model="form.description" class="form-control" name="description"></textarea>
+        </div>
+        <div class="mb-3">
+            <label>Image</label>
+            <div class="input-group">
+                <input v-model="form.image" class="form-control" name="image">
+                <ImageUpload @uploaded="form.image = $event" />
+            </div>
+        </div>
+        <div class="mb-3">
+            <label>Price</label>
+            <input v-model="form.price" type="number" class="form-control" name="image">
+        </div>
+        <button class="btn btn-outline-secondary">Save</button>
+    </form>
+</template>
+
+<script lang="ts">
+import axios from 'axios'
+import ImageUpload from '@/components/ImageUpload.vue'
+export default {
+    name: "ProductCreate",
+    components: {ImageUpload},
+    data() {
+        return {
+            form: {
+                title: '',
+                description: '',
+                image: '',
+                price: ''
+            },
+        }
+    },
+    methods: {
+        async submit() {
+            await axios.post('products', this.form)
+            await this.$router.push('/products')
+        }
+    }
+}
+
+</script>
---
+++ vue-admin/src/pages/products/ProductEdit.vue
@@ -0,0 +1,54 @@
+<template> 
+    <form @submit.prevent="submit">
+        <div class="mb-3">
+            <label>Title</label>
+            <input v-model="form.title" class="form-control" name="title">
+        </div>
+        <div class="mb-3">
+            <label>Description</label>
+            <textarea v-model="form.description" class="form-control" name="description"></textarea>
+        </div>
+        <div class="mb-3">
+            <label>Image</label>
+            <div class="input-group">
+                <input v-model="form.image" class="form-control" name="image">
+                <ImageUpload @uploaded="form.image = $event" />
+            </div>
+        </div>
+        <div class="mb-3">
+            <label>Price</label>
+            <input v-model="form.price" type="number" class="form-control" name="image">
+        </div>
+        <button class="btn btn-outline-secondary">Save</button>
+    </form>
+</template>
+
+<script lang="ts">
+import axios from 'axios'
+import ImageUpload from '@/components/ImageUpload.vue'
+export default {
+    name: "ProductEdit",
+    components: {ImageUpload},
+    data() {
+        return {
+            form: {
+                title: '',
+                description: '',
+                image: '',
+                price: ''
+            },
+        }
+    },
+    async mounted() {
+        const response = await axios.get(`products/${this.$route.params.id}`)
+        this.form = {title: response.data.title, description: response.data.description, image: response.data.image, price: response.data.price}
+    },
+    methods: {
+        async submit() {
+            await axios.put(`products/${this.$route.params.id}`, this.form)
+            await this.$router.push('/products')
+        }
+    }
+}
+
+</script>

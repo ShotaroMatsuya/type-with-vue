c4331de62a4998a2fe5ee75f78b84c6ec891efe6
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-05-17 22:38:13 +0900

    feat: create user routing

--- vue-admin/src/router/index.ts
+++ vue-admin/src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import DashboardPage from '@/pages/DashboardPage.vue';
-import UsersPage from '@/pages/users/UsersPage.vue';
+import UsersPage from '@/pages/users/Users.vue';
+import UserCreatePage from '@/pages/users/UserCreate.vue';
+import UserEditPage from '@/pages/users/UserEdit.vue';
 import RegisterPage from '@/pages/RegisterPage.vue';
 import BaseWrapper from '@/pages/BaseWrapper.vue';
 import LoginPage from '@/pages/LoginPage.vue';
@@ -14,6 +16,8 @@ const routes: Array<RouteRecordRaw> = [
     children: [
       { path: '', component: DashboardPage },
       { path: '/users', component: UsersPage },
+      { path: '/users/create', component: UserCreatePage },
+      { path: '/users/:id/edit', component: UserEditPage },
     ],
   },
 ];

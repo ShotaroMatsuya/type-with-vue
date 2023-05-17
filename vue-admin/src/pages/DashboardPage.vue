0ed6078341f8e0c5e7bfcc69cd896add47ece931
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-05-18 02:34:33 +0900

    feat: dashboard for daily sales

--- vue-admin/src/pages/DashboardPage.vue
+++ vue-admin/src/pages/DashboardPage.vue
@@ -1,3 +1,44 @@
 <template>
-  Dashboard
+  <h2>Daily Sales</h2>
+  <div id="chart"></div>
 </template>
+<script lang="ts">
+import axios from 'axios';
+import * as c3 from 'c3';
+export default {
+  name: "Dashboard",
+  data() {
+    return {}
+  },
+  async mounted() {
+    const chart = c3.generate({
+      bindto: '#chart',
+      data: {
+        x: 'x',
+        columns: [
+          ['x'], ['Sales']
+        ],
+        types: {
+          Sales: 'bar'
+        }
+      },
+      axis: {
+        x: {
+          type: 'timeseries',
+          tick: {
+            format: '%Y-%m-%d'
+          }
+        }
+      }
+    })
+    const { data } = await axios.get('chart')
+    chart.load({
+      columns: [
+        ['x', ...data.map((r: any) => r.date)],
+        ['Sales', ...data.map((r: any) => r.sum)]
+      ]
+    })
+  }
+
+}
+</script>

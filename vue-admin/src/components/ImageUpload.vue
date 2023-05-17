0335f14fe3599c72aae06c39dcc6fde8171c452f
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-05-18 00:54:19 +0900

    feat: create cmp for ImageUpload

---
+++ vue-admin/src/components/ImageUpload.vue
@@ -0,0 +1,29 @@
+<template>
+    <label class="btn btn-primary">
+        Upload <input type="file" hidden @change="upload($event)">
+    </label>
+</template>
+
+<script lang="ts">
+import axios from 'axios'
+export default {
+    name: "ImageUpload",
+    emits: ['uploaded'],
+    methods: {
+        async upload(e: Event) {
+            const {target} = e;
+            if(!(target instanceof HTMLInputElement)) return;
+            const files: FileList | null = target.files;
+            if(files == null) return;
+            const formData = new FormData();
+            formData.append('image', files[0]);
+
+            const {data} = await axios.post('upload', formData);
+
+            this.$emit('uploaded', data.url)
+        }
+    }
+}
+</script>
+
+<style scoped></style>

06331aeafe754d85fd121ea9810fa6a9862f5b37
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-05-17 23:35:42 +0900

    feat: fix and add nav link

--- vue-admin/src/components/HeaderMenu.vue
+++ vue-admin/src/components/HeaderMenu.vue
@@ -6,7 +6,7 @@
     <div class="position-sticky pt-3">
       <ul class="nav flex-column">
         <li class="nav-item">
-          <router-link to="/" active-class="active" class="nav-link">
+          <router-link to="/" exact-active-class="active" class="nav-link">
             Dashboard
           </router-link>
         </li>
@@ -18,6 +18,11 @@
         <li class="nav-item">
           <router-link to="/roles" active-class="active" class="nav-link">
             Roles
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/products" active-class="active" class="nav-link">
+            Products
           </router-link>
         </li>
       </ul>

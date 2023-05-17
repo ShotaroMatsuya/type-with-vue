0aa76c216021a84b6c5ece0e46abffec353a8af0
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-05-18 04:02:03 +0900

    perf: create vuex for user

--- vue-admin/src/store/index.ts
+++ vue-admin/src/store/index.ts
@@ -1,9 +1,17 @@
-import { createStore } from "vuex";
+import { User } from "@/models/user";
+import { Commit, createStore } from "vuex";
 
 export default createStore({
-  state: {},
+  state: {
+    user: new User(),
+  },
   getters: {},
-  mutations: {},
-  actions: {},
+  mutations: {
+    SET_USER: (state: { user: User }, user: User) => (state.user = user),
+  },
+  actions: {
+    setUser: ({ commit }: { commit: Commit }, user: User) =>
+      commit("SET_USER", user),
+  },
   modules: {},
 });
